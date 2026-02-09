7c75e5588e3e93bf703007b4f3014b7432fe8155
Douglas Yung <douglas.yung@sony.com>
2026-02-08 23:27:31 -0500

    Update clang/test/APINotes/templates.cpp for newly added Move test and refactor to make diff smaller.

--- clang/test/APINotes/templates.cpp
+++ clang/test/APINotes/templates.cpp
@@ -1,50 +1,43 @@
 // RUN: rm -rf %t && mkdir -p %t
 // RUN: %clang_cc1 -std=c++17 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/Tmpl -fdisable-module-hash -fapinotes-modules -fsyntax-only -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -x c++
-// RUN: %clang_cc1 -std=c++17 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/Tmpl -fdisable-module-hash -fapinotes-modules -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -ast-dump -ast-dump-filter Box -x c++ | FileCheck -check-prefix=CHECK-BOX17 %s
-// RUN: %clang_cc1 -std=c++17 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/Tmpl -fdisable-module-hash -fapinotes-modules -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -ast-dump -ast-dump-filter MoveOnly -x c++ | FileCheck -check-prefix=CHECK-MOVEONLY17 %s
+// RUN: %clang_cc1 -std=c++17 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/Tmpl -fdisable-module-hash -fapinotes-modules -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -ast-dump -ast-dump-filter Box -x c++ | FileCheck -check-prefix=CHECK-BOX,CHECK-BOX17 %s
+// RUN: %clang_cc1 -std=c++17 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/Tmpl -fdisable-module-hash -fapinotes-modules -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -ast-dump -ast-dump-filter MoveOnly -x c++ | FileCheck -check-prefix=CHECK-MOVEONLY,CHECK-MOVEONLY17 %s
 
 // RUN: rm -rf %t && mkdir -p %t
 // RUN: %clang_cc1 -std=c++20 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/Tmpl -fdisable-module-hash -fapinotes-modules -fsyntax-only -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -x c++
-// RUN: %clang_cc1 -std=c++20 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/Tmpl -fdisable-module-hash -fapinotes-modules -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -ast-dump -ast-dump-filter Box -x c++ | FileCheck -check-prefix=CHECK-BOX20 %s
-// RUN: %clang_cc1 -std=c++20 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/Tmpl -fdisable-module-hash -fapinotes-modules -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -ast-dump -ast-dump-filter MoveOnly -x c++ | FileCheck -check-prefix=CHECK-MOVEONLY20 %s
+// RUN: %clang_cc1 -std=c++20 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/Tmpl -fdisable-module-hash -fapinotes-modules -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -ast-dump -ast-dump-filter Box -x c++ | FileCheck -check-prefix=CHECK-BOX,CHECK-BOX20 %s
+// RUN: %clang_cc1 -std=c++20 -fmodules -fblocks -fimplicit-module-maps -fmodules-cache-path=%t/ModulesCache/Tmpl -fdisable-module-hash -fapinotes-modules -I %S/Inputs/Headers -F %S/Inputs/Frameworks %s -ast-dump -ast-dump-filter MoveOnly -x c++ | FileCheck -check-prefix=CHECK-MOVEONLY,CHECK-MOVEONLY20 %s
 
 #include "Templates.h"
 
-
-
-// CHECK-BOX17: Dumping Box:
+// CHECK-BOX: Dumping Box:
 // CHECK-BOX17-NEXT: ClassTemplateDecl {{.+}} imported in Templates Box
-// CHECK-BOX17: SwiftAttrAttr {{.+}} <<invalid sloc>> "import_owned"
-
-// Make sure the attributes aren't duplicated.
-// CHECK-BOX17-NOT: SwiftAttrAttr {{.+}} <<invalid sloc>> "import_owned"
-
-
-
-// CHECK-BOX20: Dumping Box:
 // CHECK-BOX20-NEXT: ClassTemplateDecl {{.+}} imported in Templates hidden Box
-// CHECK-BOX20: SwiftAttrAttr {{.+}} <<invalid sloc>> "import_owned"
+// CHECK-BOX: SwiftAttrAttr {{.+}} <<invalid sloc>> "import_owned"
 
 // Make sure the attributes aren't duplicated.
-// CHECK-BOX20-NOT: SwiftAttrAttr {{.+}} <<invalid sloc>> "import_owned"
+// CHECK-BOX-NOT: SwiftAttrAttr {{.+}} <<invalid sloc>> "import_owned"
 
-// CHECK-MOVEONLY17: Dumping MoveOnly:
+// CHECK-MOVEONLY: Dumping MoveOnly:
+// CHECK-MOVEONLY20-NEXT: ClassTemplateDecl {{.+}} imported in Templates hidden MoveOnly
 // CHECK-MOVEONLY17-NEXT: ClassTemplateDecl {{.+}} imported in Templates MoveOnly
-// CHECK-MOVEONLY17: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
+// CHECK-MOVEONLY: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
 
 // Make sure the attributes aren't duplicated.
-// CHECK-MOVEONLY17-NOT: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
+// CHECK-MOVEONLY-NOT: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
 
 // CHECK-MOVEONLY17: ClassTemplateSpecializationDecl {{.+}} imported in Templates {{.+}} MoveOnly
-// CHECK-MOVEONLY17: TemplateArgument type 'int'
-// CHECK-MOVEONLY17: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
+// CHECK-MOVEONLY20: ClassTemplateSpecializationDecl {{.+}} imported in Templates hidden {{.+}} MoveOnly
+// CHECK-MOVEONLY: TemplateArgument type 'int'
+// CHECK-MOVEONLY: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
 
 // Make sure the attributes aren't duplicated.
-// CHECK-MOVEONLY17-NOT: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
+// CHECK-MOVEONLY-NOT: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
 
 // CHECK-MOVEONLY17: ClassTemplateSpecializationDecl {{.+}} imported in Templates {{.+}} MoveOnly
-// CHECK-MOVEONLY17: TemplateArgument type 'float'
-// CHECK-MOVEONLY17: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
+// CHECK-MOVEONLY20: ClassTemplateSpecializationDecl {{.+}} imported in Templates hidden {{.+}} MoveOnly
+// CHECK-MOVEONLY: TemplateArgument type 'float'
+// CHECK-MOVEONLY: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
 
 // Make sure the attributes aren't duplicated.
-// CHECK-MOVEONLY17-NOT: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
+// CHECK-MOVEONLY-NOT: SwiftAttrAttr {{.+}} <<invalid sloc>> "~Copyable"
